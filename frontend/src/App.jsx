259c5e55afc6b3b40be08ba921b8e2b92234760f
Himani <himgan1611@gmail.com>
2025-04-20 14:31:29 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <h1 className="text-center">Himani ko pagal bna rakha h abhishek ne</h1>
+  );
+}
+
+export default App;
